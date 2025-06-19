c3f1c8d53943e67d55dd51edacdc87d915bc1bd2
AnushaMotamarri <motamarrianusha01@gmail.com>
2025-06-19 12:53:28 +0530

    cleanup

--- src/components/dropdown.jsx
+++ src/components/dropdown.jsx
@@ -2,6 +2,8 @@
 import React, { useEffect, useState } from 'react';
 import AsyncSelect from 'react-select/async';
 import Select from 'react-select';
+import { components } from 'react-select';
+
 // const customSingleValue = ({ data }) => (
 //   <div className="flex items-center gap-2 p-0 m-0"
 //   style={{ margin: 0, padding: 0 }}>
@@ -17,15 +19,24 @@ const CustomOption = props => {
     <div
       ref={innerRef}
       {...innerProps}
+     
       className="flex items-center gap-2 px-3 py-2 hover:bg-gray-100"
     >
       <img src={data.image} alt={data.label} className="w-5 h-5" />
-      <span className="text-black">
-        {data.name} ({data.symbol.toUpperCase()})
+      <span className="text-black"  data-testid={`coin-name-${data.name}`}>
+        {data.name} ({data.symbol?.toUpperCase()})
       </span>
     </div>
   );
 };
+const CustomControl = ({ children, ...props }) => {
+  const { selectProps } = props;
+ return <components.Control {...props} innerRef={props.innerRef} innerProps={{ ...props.innerProps, 'data-testid': selectProps.testId||'crypto-select' }}>
+    {children}
+  </components.Control>
+};
+
+
 
 const Dropdown = ({
   label,
@@ -35,6 +46,7 @@ const Dropdown = ({
   onChange,
   placeholder,
   selectFirstOptionByDefault,
+  testId
 }) => {
   const [selectedOption, setSelectedOption] = useState(null);
 
@@ -50,6 +62,7 @@ const Dropdown = ({
       {label && <label className="font-semibold">{label}</label>}
       {isAsync ? (
         <AsyncSelect
+        testId={testId}
           placeholder={placeholder}
           cacheOptions
           defaultOptions={options}
@@ -57,17 +70,19 @@ const Dropdown = ({
           getOptionLabel={opt => opt.name}
           getOptionValue={opt => opt.id}
           onChange={onChange}
-          components={{ Option: CustomOption }}
+          components={{ Option: CustomOption,Control: CustomControl }}
         />
       ) : (
         <Select
           className="text-black"
           value={selectedOption}
           options={options}
+          testId={testId}
           onChange={val => {
             setSelectedOption(val);
             onChange(val);
           }}
+          components={{ Control: CustomControl}}
         />
       )}
     </div>
