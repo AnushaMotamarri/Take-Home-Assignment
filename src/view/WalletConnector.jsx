39937c2653e75af500d75411846af7821e8a76f6
AnushaMotamarri <motamarrianusha01@gmail.com>
2025-06-19 19:51:47 +0530

    added non lazy fetch

--- src/view/WalletConnector.jsx
+++ src/view/WalletConnector.jsx
@@ -23,6 +23,20 @@ export default function WalletConnector() {
 
   return (
     <div className="p-4">
+      <h2 className="text-xl xtext-text-color">
+        <b>Connect your self-custody Wallet</b>
+      </h2>
+      <p className="text-sm py-1">
+        Connect your self-custody wallet like MetaMask or Trust Wallet to view and monitor your
+        ERC-20 token holdings in real-time. Instantly see your token balances, symbol, and asset
+        details fetched from the blockchain.
+      </p>
+      <div>
+        <span className="text-sm py-1">
+          <b>Pre-Requisite: </b>You need to have a self-custody wallet browser extension (like
+          MetaMask) installed on Chrome.
+        </span>
+      </div>
       {account ? (
         <div>
           <p className="text-green-600">
@@ -37,7 +51,7 @@ export default function WalletConnector() {
           )}
         </div>
       ) : (
-        <button onClick={connectWallet} className="!bg-primary text-white px-4 py-2 rounded">
+        <button onClick={connectWallet} className="!bg-primary my-2 text-white px-4 py-2 rounded">
           Connect Wallet
         </button>
       )}
