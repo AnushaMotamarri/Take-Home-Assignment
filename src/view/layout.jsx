ef3839594bdee88ad51764adb646fbf413c8b25a
AnushaMotamarri <motamarrianusha01@gmail.com>
2025-06-19 22:34:22 +0530

    css glitch fix

--- src/view/layout.jsx
+++ src/view/layout.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from 'react-router-dom';
-import Navbar from '../components/navbar';
+import Navbar from '../components/navBar';
 
 export default function Layout({ theme, setTheme }) {
   return (
     <div className="bg-background h-screen">
       <Navbar theme={theme} setTheme={setTheme} />
-      <div className="px-5 pt-2.5">
+      <div className="px-5 pt-2.5 h-[calc(100vh-var(--header-height))]">
         <Outlet />
       </div>
     </div>
